b9e2a6f9408730c1e841dc2f9bab9a1de1593bd8
Valkirin88 <91696211+Valkirin88@users.noreply.github.com>
2022-11-10 21:55:36 +0300

    v0.0.3
    Works with materials changing

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -5,14 +5,11 @@ public class EntryPoint : MonoBehaviour
     [SerializeField]
     private DrumsSoundsLibrary _drumsSoundsView;
     [SerializeField]
-    private LightsSet _lightsSet;
-    [SerializeField]
     private HitEffectView _hitEffectView;
 
     private InputManager _inputManager;
     private AudioSource _audioSource;
     private DrumController _drumController;
-    private LightController _lightController;
     private HitEffectManager _hitEffectManager;
 
     private void Awake()
@@ -22,8 +19,6 @@ public class EntryPoint : MonoBehaviour
         _audioSource = _drumsSoundsView.GetComponent<AudioSource>();
         
         _drumController = new DrumController(_drumsSoundsView, _audioSource, _inputManager);
-
-       // _lightController = new LightController(_lightsSet, _inputManager);
 
         _hitEffectManager = new HitEffectManager(_hitEffectView, _inputManager);
     }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,62 +7,52 @@ public class InputManager
 
     private RaycastHit[] _hit;
     private Drum _drum;
-    private Touch _touch;
-
+    
     public InputManager()
     {
-
-#if UNITY_EDITOR
         _hit = new RaycastHit[5];
-#endif
-
-
-#if UNITY_ANDROID
-        _hit = new RaycastHit[(int)_touch.maximumPossiblePressure];
-#endif
-
     }
-
-    //public void Update()
-    //{
-    //    if (Input.touchCount > 0)
-    //    {
-    //        for (int i = 0; i < Input.touchCount; i++)
-    //        {
-    //            if (Input.GetTouch(i).phase == TouchPhase.Began)
-    //            {
-    //                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
-    //                if (Physics.Raycast(ray, out _hit[i]))
-    //                {
-    //                    _drum = _hit[i].transform.GetComponent<Drum>();
-
-    //                    if (_drum)
-    //                    {
-    //                        GetDrum?.Invoke(_drum);
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
 
     public void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount > 0)
         {
-           {
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                    if (Physics.Raycast(ray, out _hit[0]))
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Began)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                    if (Physics.Raycast(ray, out _hit[i]))
                     {
-                        _drum = _hit[0].transform.GetComponent<Drum>();
+                        _drum = _hit[i].transform.GetComponent<Drum>();
 
                         if (_drum)
                         {
                             GetDrum?.Invoke(_drum);
                         }
                     }
-                
-           }
+                }
+            }
         }
     }
+
+    //public void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //       {
+    //                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //                if (Physics.Raycast(ray, out _hit[0]))
+    //                {
+    //                    _drum = _hit[0].transform.GetComponent<Drum>();
+
+    //                    if (_drum)
+    //                    {
+    //                        GetDrum?.Invoke(_drum);
+    //                    }
+    //                }
+                
+    //       }
+    //    }
+    //}
 }
