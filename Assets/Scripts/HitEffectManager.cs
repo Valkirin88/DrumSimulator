cf6afff8e5ca98ef1d13fc522934b3b3bc55ea34
Valkirin88 <91696211+Valkirin88@users.noreply.github.com>
2022-11-13 16:11:59 +0300

    v0.0.4
    Added coroutine instaed async/await. Now works!

--- Assets/Scripts/HitEffectManager.cs
+++ Assets/Scripts/HitEffectManager.cs
@@ -1,10 +1,8 @@
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class HitEffectManager 
 {
     private InputManager _inputManager;
-    private int _timeDelay = 100;
     private HitEffectView _hitEffectView;
     private Material _material;
    
@@ -18,15 +16,7 @@ public class HitEffectManager
 
     private void ChangeDrumMaterial(Drum drum)
     {
-        _material = drum.GetComponent<MeshRenderer>().material;
         drum.GetComponent<MeshRenderer>().material = _hitEffectView.LightMaterial;
-        Debug.Log(drum);
-        TimeDelay(drum);
-    }
-
-    private async void TimeDelay(Drum drum)
-    {
-        await Task.Delay(_timeDelay);
-        drum.GetComponent<MeshRenderer>().material = _hitEffectView.TransperentMaterial;
-    }
+        _hitEffectView.TimeDelay(drum);
+    } 
 }
--- Assets/Scripts/HitEffectView.cs
+++ Assets/Scripts/HitEffectView.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class HitEffectView: MonoBehaviour
@@ -7,6 +8,19 @@ public class HitEffectView: MonoBehaviour
     [SerializeField]
     private Material _lightMaterial;
 
+    private float _timeDelay = 0.1f;
+
     public Material TransperentMaterial => _transperentMaterial; 
-    public Material LightMaterial  => _lightMaterial; 
+    public Material LightMaterial  => _lightMaterial;
+
+    public void TimeDelay(Drum drum)
+    {
+        StartCoroutine(TimeDelayC(drum));
+    }
+
+    public IEnumerator TimeDelayC(Drum drum)
+    {
+        yield return new WaitForSeconds(_timeDelay);
+        drum.GetComponent<MeshRenderer>().material = TransperentMaterial;
+    }
 }
